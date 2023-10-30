5a7393198d0d6f16c71c4442d311fe1b45705794
Obolo Emmanuel <e.obolo@alustudent.com>
2023-10-30 09:54:21 -0700

    documentation fixed

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,14 +2,18 @@
 """
 Using what you did in the task #0,
 extend your Python script to export
-data in the CSV format.
+data in the JSON format.
 
 Requirements:
 
-Records all tasks that are owned by this employee
-Format must be:
-"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv
+Records all tasks that are owned by
+this employee Format must be: { "USER_ID":
+[{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username": "USERNAME"},
+{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username":
+"USERNAME"}, ... ]} File name must be:
+USER_ID.json
 """
 if __name__ == "__main__":
     import csv
